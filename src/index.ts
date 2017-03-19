5e1f62e504ddd524e3d5f5215834a1dec7be4e0c
Maxfield Walker <maxfield.walker@gmail.com>
2017-03-19 19:16:16 +0900

    add MemoryRange interface

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,4 @@ import * as jisx0201 from "./helpers/jisx0201";
 export { jisx0201 };
 export { GR, stringToGR, grToString, grsInfo, GRInfo, GRType } from "./gr";
 export { ArgumentType, InstructionInfo, instructionsInfo, InstructionType } from "./instructions";
+export { MemoryRange } from "./memoryRange";
